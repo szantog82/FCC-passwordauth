dcb14a974e30d3c5c9395bdde8e3b86534b0363e
Gábor Szántó <szantog82@gmail.com>
2016-10-21 09:40:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var bcrypt = require('bcrypt');
 var mongodb = require('mongodb');
 
 var d = new Date();
- 
+ var loginmatch = false;
+ var passwordmatch = false;
+ var position = 0;
 
 var uri = '';
 
@@ -17,40 +19,58 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + "/views/index.html")
 });
 
+
 app.get('/signup', function(req, res){
   res.sendFile(__dirname + "/views/sign-up.html")
 });
 
 app.post('/signup', function(req, res){
-  var hashedlogin = bcrypt.hashSync(req.body.login, 10);
-  var hashedpassword = bcrypt.hashSync(req.body.password, 10);
-  console.log(hashedlogin)
+  loginmatch = false;
+  passwordmatch = false;
+  var login = req.body.login;
+  var password = req.body.password;
+  var hashedlogin = bcrypt.hashSync(login, 10);
+var hashedpassword = bcrypt.hashSync(password, 10);
   mongodb.MongoClient.connect(uri, function(err, db) {
             if (err) throw err;
 var users = db.collection('users');
-users.find({
-                "login": hashedlogin
-            }).toArray(function(err, data) {
-                if (JSON.stringify(data).length < 3) {            //The login name does not exist
+users.find({}).toArray(function(err, data) {
+  var input = data;
+  console.log("fetching datas")
+              for (var i = 0; i < input.length; i++) {
+               console.log(input[i].login)
+                if (bcrypt.compareSync(login, input[i].login)) {loginmatch = true; position = i;
+              }
+                
+              }
+              if (loginmatch) {
+                if (bcrypt.compareSync(password, input[position].password)){
+                 res.end("password && login matches");
+                db.close();} 
+                
+                else{
+                res.end("password mismatch");
+                db.close();}
+              }
+              
+              else {
                  users.insert({
                         "login": hashedlogin,
                         "password": hashedpassword,
                         "date": d.toLocaleString()
 });
                  
-                  res.end("new entry created");
+                  res.send("new entry created");
                   console.log("new entry created");
-                  db.close();
 }
-else{
-  console.log("this username already exists, choose another one!")
-  res.end("this username already exists, choose another one!")
+
   db.close();
-}
 });
 });
-  res.end()
 });
+
+
+
 
 app.post('/api', function(req, res){
   console.log(JSON.stringify(req.body))
